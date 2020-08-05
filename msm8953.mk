587230b729e44314cfd3c22a77a2e1eb835eae24
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-08-06 00:01:56 +0200

    bluetooth: Switch to QTI Bluetooth
    Change-Id: I874ebce69ed6b8e8a596623b68af629dff1d6fb4

--- msm8953.mk
+++ msm8953.mk
@@ -97,6 +97,7 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
+    BluetoothQti \
     liba2dpoffload \
     libaacwrapper \
     libaudio-resampler \
@@ -112,6 +113,7 @@ PRODUCT_COPY_FILES += \
     hardware/qcom-caf/msm8996/audio/configs/msm8953/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    hardware/qcom-caf/msm8996/audio/configs/common/bluetooth_qti_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_qti_audio_policy_configuration.xml \
     hardware/qcom-caf/msm8996/audio/configs/msm8953/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     hardware/qcom-caf/msm8996/audio/configs/msm8953/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     hardware/qcom-caf/msm8996/audio/configs/msm8953/sound_trigger_mixer_paths_wcd9306.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_wcd9306.xml \
@@ -142,7 +144,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
     libbt-vendor \
-    libbase_shim
+    libbase_shim \
+    libbthost_if
 
 # Boot control
 PRODUCT_PACKAGES += \
